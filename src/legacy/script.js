cf94e703e46cfb77e1d46342a17f4f59cb62fcae
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-03 16:42:44 +0000

    app works in react

--- src/legacy/script.js
+++ src/legacy/script.js
@@ -1,31 +1,36 @@
-const randomNumber = Math.floor(Math.random() * 8);
+import React from 'react';
+export default function Quotes() {
+  const randomNumber = Math.floor(Math.random() * 8);
 
-let quote = "";
+  let quote = "";
 
-switch(randomNumber) {
-  case 0:
-    quote = "<h1>You miss 100% of the shots you don't take</h1> <p>Wayne Gretzky - Hockey's greatest ever player</p>";
+  switch(randomNumber) {
+    case 0:
+      quote = "You miss 100% of the shots you don't take ayne Gretzky - Hockey's greatest ever player";
+      break;
+    case 1:
+      quote = 'Every time I practice, the luckier I get ary Player - Golfing legend';
+      break;
+    case 2:
+      quote = "Winning isn't everything, but wanting to win is nce Lombardi - NFL player and coach";
+      break;
+    case 3:
+      quote = "I've failed over and over again in my life. And that is why I succeed ichael Jordan - NBA legend";
+      break;
+    case 4:
+      quote = "The more difficult the victory, the greater the happiness in winning. ele - The greatest football player ever";
+      break;
+    case 5:
+      quote = 'Champions keep playing till they get it right illie Jean King - Tennis legend';
+      break;
+        case 6:
+      quote = 'You are never a loser until you stop trying Mike Ditka - former NFL player';
+      break;
+    case 7:
+      quote = 'Half the game is mental, the other half is being mental im McKenny - Hockey player';
     break;
-  case 1:
-    quote = '<h1>Every time I practice, the luckier I get</h1> <p>Gary Player - Golfing legend</p>';
-    break;
-  case 2:
-    quote = "<h1>Winning isn't everything, but wanting to win is</h1><p>Vince Lombardi - NFL player and coach</p>";
-    break;
-  case 3:
-    quote = "<h1>I've failed over and over again in my life. And that is why I succeed</h1> <p>Michael Jordan - NBA legend</p>";
-    break;
-  case 4:
-    quote = "<h1>The more difficult the victory, the greater the happiness in winning.</h1> <p>Pele - The greatest football player ever</p>";
-    break;
-  case 5:
-    quote = '<h1>Champions keep playing till they get it right</h1> <p>Billie Jean King - Tennis legend</p>';
-    break;
-      case 6:
-    quote = '<h1>You are never a loser until you stop trying</h1> <p>Mike Ditka - former NFL player</p>';
-    break;
-  case 7:
-    quote = '<h1>Half the game is mental, the other half is being mental</h1> <p>Jim McKenny - Hockey player</p>';
-	 break;
+  }
+  return (
+    quote
+  );
 }
-document.write(`${quote}`);
