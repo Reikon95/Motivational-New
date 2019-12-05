7963368c4277b852301a7bc4222ec14517055e4c
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-05 18:58:58 +0000

    statement with objects fully operational

--- src/legacy/script.js
+++ src/legacy/script.js
@@ -1,33 +1,44 @@
 import React from 'react';
 export default function Quotes() {
-  const randomNumber = Math.floor(Math.random() * 3);
+  const randomNumber = Math.floor(Math.random() * 8);
 
   let quote = "";
 
   switch(randomNumber) {
     case 0:
-      quote = {words: "You miss 100% of the shots you don't take", person: 'Wayne Gretzky', description: 'NHL all time record goalscorer', wiki: 'https://en.wikipedia.org/wiki/Wayne_Gretzky'};
+      quote = {words: "You miss 100% of the shots you don't take", person: 'Wayne Gretzky', 
+      description: 'NHL all time record goalscorer', wiki: 'https://en.wikipedia.org/wiki/Wayne_Gretzky'};
       break;
     case 1:
-      quote = {words: 'Every time I practice, the luckier I get', person: 'Gary Player', description: 'Golfing legend - three time masters winner', wiki: 'https://en.wikipedia.org/wiki/Gary_Player'};
+      quote = {words: 'Every time I practice, the luckier I get', person: 'Gary Player', 
+      description: 'Golfing legend - three time masters winner', wiki: 'https://en.wikipedia.org/wiki/Gary_Player'};
       break;
     case 2:
-      quote = {words: "Winning isn't everything, but wanting to win is", person: 'Vince Lombardi', description: 'NFL player and coach', wiki: 'https://en.wikipedia.org/wiki/Vince_Lombardi'};
+      quote = {words: "Winning isn't everything, but wanting to win is", person: 'Vince Lombardi', 
+      description: 'NFL player and coach', wiki: 'https://en.wikipedia.org/wiki/Vince_Lombardi'};
       break;
     case 3:
-      quote = "I've failed over and over again in my life. And that is why I succeed ichael Jordan - NBA legend";
+      quote = {words: "I've failed over and over again in my life. And that is why I succeed", person: 'Michael Jordan',
+      description: 'NBA legend', wiki: 'https://en.wikipedia.org/wiki/Michael_Jordan'};
       break;
     case 4:
-      quote = "The more difficult the victory, the greater the happiness in winning. ele - The greatest football player ever";
+      quote = {words: "The more difficult the victory, the greater the happiness in winning", person: 'Pele',
+      description: 'Football great', wiki: 'https://en.wikipedia.org/wiki/Pele'};
       break;
     case 5:
-      quote = 'Champions keep playing till they get it right illie Jean King - Tennis legend';
+      quote = {words: 'Champions keep playing till they get it right', person: 'Billie Jean King',
+      description: 'Tennis legend', wiki: 'https://en.wikipedia.org/wiki/Billie_Jean_King'};
       break;
-        case 6:
-      quote = 'You are never a loser until you stop trying Mike Ditka - former NFL player';
+    case 6:
+      quote = {words: 'You are never a loser until you stop trying', person: 'Mike Ditka',
+      description: 'former NFL player', wiki: 'https://en.wikipedia.org/wiki/Mike_Ditka'};
       break;
     case 7:
-      quote = 'Half the game is mental, the other half is being mental Jim McKenny - Hockey player';
+      quote = {words: 'Half the game is mental, the other half is being mental', person: 'Jim McKenny',
+      description: 'Hockey player', wiki: 'https://en.wikipedia.org/wiki/Jim_McKenny'};
+    break;
+    default: quote = {words: "You miss 100% of the shots you don't take", person: 'Wayne Gretzky', 
+    description: 'NHL all time record goalscorer', wiki: 'https://en.wikipedia.org/wiki/Wayne_Gretzky'};
     break;
   }
   return (
